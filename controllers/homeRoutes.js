6a78a3777f6659b1ea59d872917e83a694bc1a39
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-11-08 22:37:02 -0600

    corrected home route to show user posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,27 +4,32 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-      // Get all projects and JOIN with user data
+      // Get all posts and JOIN with user data
+      // const userData = await User.findAll({});
+      const user = req.session.user_id;
       const postData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['title'],
-          },
-        ],
+        // include: [
+        //   {
+        //     model: User,
+        //     attributes: ['title'],
+        //   },
+        // ],
       });
-  
+      const postsByUser = postData.filter(post => post.user_id === user);
       // Serialize data so the template can read it
-      const posts = postData.map((post) => post.get({ plain: true }));
+      // const posts = postData.map((post) => post.get({ plain: true }));
   
       // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        posts, 
-        logged_in: req.session.logged_in 
-      });
+      // res.render('homepage', { 
+      //   posts: postsByUser, 
+      //   logged_in: req.session.logged_in 
+      // });
+      res.json(postsByUser);
+
     } catch (err) {
       res.status(500).json(err);
     }
+    
   });
 
   router.get('/post/:id', async (req, res) => {
